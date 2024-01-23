3ae1dff03fbd4f3b50136cd856e88965d27fd09b
yung-khajiit <jeenriquez1219@gmail.com>
2024-01-23 08:53:33 -0600

    Added category

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,21 @@ import {ref, transformVNodeArgs} from 'vue'
 const todos = ref([])
 const name = ref('')
 const input_content = ref('')
+const input_category = ref(null)
+
 
 const addTodo = () => {
-  if(input_content.value.trim() ==='') {
+  if(input_content.value.trim() === '' || input_category.value == null) {
     return
   }
   todos.value.push({
     content: input_content.value,
+    category: input_category.value,
   })
 
   input_content.value = ''
-  // console.log(todos)
+  input_category.value = null
+  //console.log(todos)
 
 }
 
@@ -35,6 +39,24 @@ const addTodo = () => {
       <h4>What's on your todo list?</h4>
       <input type="text" placeholder="e.g Make a video" v-model="input_content" />
       <!-- {{ input_content }} -->
+
+      <h4>Pick a Category</h4>
+      <div class="options">
+        <label>
+          <input type="radio" name="category" value="business" v-model="input_category" />
+          <span class="bubble business"></span>
+          <div>Business</div>
+        </label>
+
+        <label>
+          <input type="radio" name="category" value="personal" v-model="input_category" />
+          <span class="bubble personal"></span>
+          <div>Personal</div>
+        </label>
+
+        <!-- {{ input_category }} -->
+
+      </div>
      </form> 
     </section>
 
